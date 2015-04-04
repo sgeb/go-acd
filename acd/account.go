dcbb9e219957f3c8f191d752149a8767c92ba812
Serge Gebhardt <serge.gebhardt@gmail.com>
2015-04-05 01:07:51 +0200

    Add /account/quota call

--- acd/account.go
+++ acd/account.go
@@ -5,7 +5,10 @@
 
 package acd
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+)
 
 // AccountService provides access to the account related functions
 // in the Amazon Cloud Drive API.
@@ -21,6 +24,8 @@ type AccountInfo struct {
 	Status     *string `json:"status"`
 }
 
+// Provides information about the current user account like the status and the
+// accepted “Terms Of Use”.
 func (s *AccountService) GetInfo() (*AccountInfo, *http.Response, error) {
 	req, err := s.client.NewRequest("GET", "account/info", nil)
 	if err != nil {
@@ -34,4 +39,27 @@ func (s *AccountService) GetInfo() (*AccountInfo, *http.Response, error) {
 	}
 
 	return accountInfo, resp, err
+}
+
+// AccountQuota represents information about the account quotas.
+type AccountQuota struct {
+	Quota          *uint64    `json:"quota"`
+	LastCalculated *time.Time `json:"lastCalculated"`
+	Available      *uint64    `json:"available"`
+}
+
+// Gets account quota and storage availability information.
+func (s *AccountService) GetQuota() (*AccountQuota, *http.Response, error) {
+	req, err := s.client.NewRequest("GET", "account/quota", nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	accountQuota := new(AccountQuota)
+	resp, err := s.client.Do(req, accountQuota)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return accountQuota, resp, err
 }
